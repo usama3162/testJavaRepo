675e0fbc9c024f571cc73e405a450975add6695e
Usama Sajjad <usama.sajjad401@gmail.com>
2021-09-04 20:16:16 +0500

    whitespace  [ci skip]

--- src/BookDao.java
+++ src/BookDao.java
@@ -7,7 +7,7 @@ public static int save(String callno,String name,String author,String publisher,
 	try{
 		Connection con=DB.getConnection();
 		PreparedStatement ps=con.prepareStatement("insert into books(callno,name,author,publisher,quantity) values(?,?,?,?,?)");
-		ps.setString(1,callno);
+		ps.setString(1, callno);
 		ps.setString(2,name);
 		ps.setString(3,author);
 		ps.setString(4,publisher);
